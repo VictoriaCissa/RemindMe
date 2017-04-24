476aaa6974fa43d17fcdd7953c4c75d9a4c78417
Victoria <kyssa.viktoria@mail.ru>
2017-04-24 17:51:20 +0300

    Create MainActivity class and layout

---
+++ app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -0,0 +1,19 @@
+package com.qoobico.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Victoria on 24.04.2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
